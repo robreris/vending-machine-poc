79976aa9ab2a9355fad8c489438c766be2f325c9
Robert Reris <rreris@fortinet.com>
2025-08-15 13:47:31 -0700

    testing

---
+++ apps/vm-poc-backend-math/terraform/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = ">= 5.0" }
+  }
+}
+
+provider "aws" {}
+
+module "ecr" {
+  source = "../../../modules/microservice-ecr"
+  #name   = "vm-poc-backend-math"                
+  name   = yamldecode(file("${path.root}/../values.yaml")).name
+}
+
+output "repository_url" {
+  value = module.ecr.repository_url
+}
