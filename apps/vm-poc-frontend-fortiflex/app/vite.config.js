c6fab465bde9d11f68e3e4cbd8aa061a80b63321
Robert Reris <rreris@fortinet.com>
2025-08-26 12:30:47 -0700

    testing

--- apps/vm-poc-frontend-fortiflex/app/vite.config.js
+++ apps/vm-poc-frontend-fortiflex/app/vite.config.js
@@ -5,14 +5,10 @@ import fs from 'fs';
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync('./certs/key.pem'),
-      cert: fs.readFileSync('./certs/cert.pem'),
-    },
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://ec2-52-43-126-239.us-west-2.compute.amazonaws.com:8000',
+        target: 'https://fortiflex.robs-fortinet-apps.com:8000',
         changeOrigin: true,
         secure: false, //self-signed certs
       },
