dc99a8a8be6bb91ea97b2f654fc67945bb42e905
Robert Reris <rreris@fortinet.com>
2025-08-26 15:04:09 -0700

    trying nginx in fortiflex frontend

--- apps/vm-poc-frontend-fortiflex/app/vite.config.js
+++ apps/vm-poc-frontend-fortiflex/app/vite.config.js
@@ -1,20 +1,23 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import os from 'os';
 
-FORTIFLEX_BACKEND_URL = os.environ.get("FORTIFLEX_BACKEND_URL", "http://vm-poc-backend-fortiflex:5000/") 
+export default defineConfig(() => {
+  const target =
+    process.env.FORTIFLEX_BACKEND_URL ||
+    'http://vm-poc-backend-fortiflex:5000';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: FORTIFLEX_BACKEND_URL,
-        changeOrigin: true,
-        secure: false, //self-signed certs
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  }
+  };
 });
